e6eeca8d16ae431ea3d1e815e4000b57e07b58d6
everettsmith928 <everett.marsland.smith@gmail.com>
2023-09-12 16:57:52 -0600

    Vue Exercise

--- src/components/ExpressionsExercise.vue
+++ src/components/ExpressionsExercise.vue
@@ -9,16 +9,32 @@
             below:
           </p>
 
-          <p>{{}}</p>
+          <p>{{ x + y }}</p>
         </div>
         <div class="border p-1">
           <p>
             Use interpolation to get the customary Hello, World! to display on
             the screen. Don't forget your quotes
           </p>
-          <p>{{}}</p>
+          <p>{{ greeting }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const x = 100
+    const y = 100
+    const greeting = 'Hello World!'
+    return {
+      x,
+      y,
+      greeting
+    }
+  }
+
+}
+</script>
